3e1d6b6388847b966c8e5df12c1c7fd77045de37
Alexsandro <alexsandroocanha@gmail.com>
2025-06-24 22:01:10 -0400

    Terraform v2

--- providers.tf
+++ providers.tf
@@ -6,10 +6,23 @@ terraform {
       version = "6.0.0"
     }
   }
+  backend "s3" {
+    bucket  = "rocketseat-state-alex"
+    region  = "us-east-2"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
 }
 
 provider "aws" {
   region  = "us-east-2" 
-  profile = "Alex"
+  profile = "Alface"
 }
 
+
+resource "aws_s3_bucket" "state_terraform" {
+  bucket = var.state_bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
